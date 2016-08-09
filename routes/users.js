f1f142c16a3e869f8b556df22f136aa9b0fff3cc
Georgia Mills <georgee_123@hotmail.com>
2016-08-09 14:05:10 +0100

    user form online

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('userNew', { title: 'title' })
+});
+
+router.get('/new/pageholder', function(req, res, next) {
+  res.send('HELO')
+});
+
 module.exports = router;
